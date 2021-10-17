c2eb7e552e669815da7dd0014fca07d5e24aa49a
Henrique Boiteux <henriqueboiteux62@gmail.com>
2021-10-17 17:30:45 -0300

    Commit Slide

---
+++ js/script.js
@@ -0,0 +1,5 @@
+new SimpleSlide({
+    slide: 'portifolio', // nome do atributo data-slide="principal"
+    auto: true, // se o slide deve passar automaticamente
+    time: 5000 // tempo de transição dos slides
+})
